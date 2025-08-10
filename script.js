d5833c1727e1ee564f31a26e97b48a1d2bc622c0
unknown <yassine59958@gmail.com>
2025-08-10 22:01:55 +0200

    request working

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const apiKey = '2abfbeff36b8b04bb84bafc2903e2e9a';
-const city = 'Paris'; // pour tester
+
+function getData() {
+    const apiKey = '2abfbeff36b8b04bb84bafc2903e2e9a';
+const city = document.getElementById('city').value;
 
 const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+console.log(city)
 
 fetch(currentWeatherUrl)
   .then(response => {
@@ -12,9 +16,12 @@ fetch(currentWeatherUrl)
   })
   .then(data => {
     console.log(data);
-    console.log(`Température : ${data.main.temp}°C`);
+    let degre = data.main.temp - 273.15;
+    console.log(`Température : ${degre}°C`);
     console.log(`Météo : ${data.weather[0].description}`);
   })
   .catch(error => {
     console.error("Erreur lors de l'appel à l'API :", error);
   });
+
+}
